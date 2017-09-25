45b942b774b819c6b1f960cdf6e5890dd2a9e2cc
alpbourne <alpbourne@gmail.com>
2017-09-25 22:34:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,9 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if !genre_count.include?(genre)
-        genre_count << genre
+        genre_count(genre) += 1
+      else 
+        genre_count(genre) = 1
       end
     end
   end
